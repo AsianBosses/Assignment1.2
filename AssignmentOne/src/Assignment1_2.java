894196c2fdbd16463cb2705d55ad156cd81c70b7
Cuong Dinh <Cuong Dinh@Cuong>
2015-09-09 16:42:41 -0400

    initial

---
+++ AssignmentOne/src/Assignment1_2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Assignment1_2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String username, password;
+		System.out.println("Enter your username: ");
+		username = scan.nextLine();
+		System.out.println("Enter your password: ");
+		password = scan.nextLine();
+		System.out.println("Hello " + username + ", Welcome to CSC 200 class! Your password is XXXXXX!");
+
+	}
+}
